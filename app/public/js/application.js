4af2f703a9c96728906b2200bc4592b1e3a12f85
Ben Goldstein <email": null>
2016-06-23 16:00:50 -0700

    can drag geolocator marker to output lat/lng to console.

--- app/public/js/application.js
+++ app/public/js/application.js
@@ -3,7 +3,7 @@ var styleArray = [];
 var myLatLng = {lat: 37.784580, lng: -122.397437};
 
 function initMap() {
-
+  var markerSpot;
   map = new google.maps.Map(document.getElementById('map'), {
     center: myLatLng,
     zoom: 10,
@@ -19,9 +19,10 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
+      markerSpot = pos;
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('Drag and Drop to print lat/lng to console.');
       map.setCenter(pos);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
@@ -29,7 +30,21 @@ function initMap() {
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
+    myLatLng = {lat: 37.784580, lng: -122.397437};
   }
+
+  marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Drag Me!',
+    draggable: true
+  });
+  google.maps.event.addListener(marker, 'dragend', function (event) {
+    var lat = event.latLng.lat();
+    var long = event.latLng.lng();
+    var latlng = {lat: lat, lng: long};
+    console.log(latlng)
+  });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
