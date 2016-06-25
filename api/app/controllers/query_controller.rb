81d0a0411fac007462da9af95a2564962aae9795
Ben Goldstein <email": null>
2016-06-25 14:49:46 -0700

    update api query to make second call still a work in progress and too slow, create css animation for header/title and restructure header into word divs

--- api/app/controllers/query_controller.rb
+++ api/app/controllers/query_controller.rb
@@ -8,6 +8,16 @@ class QueryController < ApplicationController
     long = params[:long]
     url = "https://wdq.wmflabs.org/api?q=CLAIM[31:178561]%20and%20between[582,#{start_year},#{end_year}]%20and%20around[625,#{lat},#{long},#{radius}]"
     response = HTTParty.get(url)
-    render json: response
-  end
+    qIDS = response['items']
+    qIDString = qIDS.map{|id| 'Q'+id.to_s}.join("%7C")
+
+    mediaUrl = "https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=#{qIDString}&props=labels%7Cdescriptions%7Cclaims%7Csitelinks%2Furls&languages=en&languagefallback=1&sitefilter=&formatversion=2"
+    media_response = HTTParty.get(mediaUrl)
+
+    # parsed_response = media_response.entities.map do
+
+
+        render json: media_response
+    # end
+    end
 end
