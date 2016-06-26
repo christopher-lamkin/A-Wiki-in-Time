f70f6b848d71234f1038162ed7cc6ac5e155bfc5
Chris Lamkin <email": null>
2016-06-26 12:12:25 -0700

    modify event migration to include qID, create Event objects after API request

--- api/app/controllers/query_controller.rb
+++ api/app/controllers/query_controller.rb
@@ -15,16 +15,23 @@ class QueryController < ApplicationController
     media_response = HTTParty.get(mediaUrl)
     # p first_battle_title = media_response['entities']["Q#{qIDS[0]}"]['labels']['en']['value']
 
+    Query.create(user_id: current_user, query_url: url, latitude: lat, longitude: long, start_date: start_year, end_date: end_year, radius: radius,    )
+
     parsed_response = media_response['entities'].map do |entity, value|
             # binding.pry
         {entity => {
             title: value['labels']['en']['value'],
-            # description: value['descriptions']['en']['value'],
+            description: value['descriptions']['en']['value'],
             latitude: value['claims']['P625'][0]['mainsnak']['datavalue']['value']['latitude'],
             longitude: value['claims']['P625'][0]['mainsnak']['datavalue']['value']['longitude'],
             end_time: value['claims']['P582'][0]['mainsnak']['datavalue']['value']['time'],
             link: value['sitelinks']['enwiki']['url']
         }}
+    end
+    parsed_response.each do |entity|
+        entity.each do |qID, value|
+            Event.create(qID: qID, title: value['title'], description: value['description'], date: value['end_time'],latitude: value['latitude'], longitude: value['longitude'], event_url: value['link'] )
+        end
     end
     parsed_response.unshift({'qids' => qIDS})
     render json: parsed_response
