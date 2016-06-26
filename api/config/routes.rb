d173a3458814e0b9e35fb5f4566656ad450eba11
Raymond Sapida <ray.sapida@gmail.com>
2016-06-26 10:21:54 -0700

    Implement authentication with devise and jwt

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # devise_for :users
   post 'query', to: 'query#create'
+  post 'auth_user' => 'authentication#authenticate_user'
+  get 'home' => 'home#index'
 end
