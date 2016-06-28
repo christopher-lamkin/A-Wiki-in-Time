b7505563c7c4e2236bf3382801d5d39f3b1b1d94
Olya Royall <email": null>
2016-06-27 18:23:22 -0700

    uncomment api call script and update logic for map rendering

--- front-end/app/components/Home.js
+++ front-end/app/components/Home.js
@@ -2,11 +2,7 @@ var React = require('react');
 var SubmitForm = require('../components/SubmitForm');
 var Gmap = require('../components/Gmap');
 var initialCenter = {lat: 37.784580, lng: -122.397437};
-var GmapWrapper = require('../components/GmapWrapper');
 
-function onLoad() {
-  console.log('script loaded');
-}
 
 var Home = React.createClass({
   getInitialState() {
@@ -15,19 +11,19 @@ var Home = React.createClass({
     }
   },
 
-  // componentDidMount: function(){
-  //   if (google) {
-  //     this.setState({isLoaded: true});
-  //   }
-  // },
+  componentWillMount() {
+    if(google) {
+      this.setState({isLoaded: true});
+    }
+  },
 
   render() {
-        return (
-          <div style={{width: '100%', height: '100%'}}>
-            <GmapWrapper asyncScriptOnLoad={onLoad} initialCenter={initialCenter} />
+      return this.state.isLoaded === false
+      ? <div>loading...</div>
+    : <div style={{width: '100%', height: '100%'}}>
+            <Gmap initialCenter={initialCenter} />
             <SubmitForm />
           </div>
-        )
   }
 });
 
