ffdc3420cf45e65823442070de290370b1911a07
Olya Royall <email": null>
2016-06-28 18:19:00 -0700

    add images for texture in the images folder

--- front-end/app/containers/NavBarContainer.js
+++ front-end/app/containers/NavBarContainer.js
@@ -15,12 +15,14 @@ var NavBarContainer = React.createClass({
 
   render(){
     return(
-      <div id='header'>
-        <div className='header-div'><a href='/'>A </a></div>
-        <div className='header-div' id='wiki-header'><a href='/'>WiKi</a></div>
-        <div className='header-div'><a href='/'>in </a></div>
-        <div className='header-div'><a href='/'>Time</a></div>
-      </div>
+      <nav className="navbar-nav navbar-fixed-top" style={{backgroundColor: "#F3E9D2"}}>
+        <div id='header'>
+          <div className='header-div'><a href='/'>A </a></div>
+          <div className='header-div' id='wiki-header'><a href='/'>WiKi</a></div>
+          <div className='header-div'><a href='/'>in </a></div>
+          <div className='header-div'><a href='/'>Time</a></div>
+        </div>
+      </nav>
     )
   }
 
