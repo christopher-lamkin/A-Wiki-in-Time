26e6d803cdc7f99e130b447c23da81fa20665157
Olya Royall <email": null>
2016-06-28 21:36:11 -0700

    update height and margins for wiki header

--- front-end/app/components/Main.js
+++ front-end/app/components/Main.js
@@ -3,6 +3,24 @@ var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 require('../main.css');
 var NavBarContainer = require('../containers/NavBarContainer');
 
+// var WindowDimensions = React.createClass({
+//     render: function() {
+//         return <span>{this.state.width} x {this.state.height}</span>;
+//     },
+//     updateDimensions: function() {
+//         this.setState({width: $(window).width(), height: $(window).height()});
+//     },
+//     componentWillMount: function() {
+//         this.updateDimensions();
+//     },
+//     componentDidMount: function() {
+//         window.addEventListener("resize", this.updateDimensions);
+//     },
+//     componentWillUnmount: function() {
+//         window.removeEventListener("resize", this.updateDimensions);
+//     }
+// });
+
 var Main = React.createClass({
   render() {
     return (
--- front-end/app/containers/NavBarContainer.js
+++ front-end/app/containers/NavBarContainer.js
@@ -18,10 +18,10 @@ var NavBarContainer = React.createClass({
       <nav className="navbar">
         <div id='header' className="nav">
           <div className="container">
-            <div className='header-div'><a href='/'>A </a></div>
-            <div className='header-div' id='wiki-header'><a href='/'>WiKi</a></div>
-            <div className='header-div'><a href='/'>in </a></div>
-            <div className='header-div'><a href='/'>Time</a></div>
+            <span className='header-div'><a href='/'>A </a></span>
+            <span className='header-div' id='wiki-header'><a href='/'>WiKi</a></span>
+            <span className='header-div'><a href='/'>in </a></span>
+            <span className='header-div'><a href='/'>Time</a></span>
           </div>
         </div>
       </nav>
